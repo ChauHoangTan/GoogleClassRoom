c549f567b1f18bb4ca1b3392806a79a538f2860f
ChauHoangTan <chauhoangtan6937@gmail.com>
2023-11-12 14:35:16 +0700

    header

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -1,28 +1,96 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { Avatar, Stack } from '@mui/material';
+import {Link, useNavigate} from 'react-router-dom'
 import logo from '../assets/img/logo.png'
+import './style.scss'
 
-function Header() {
-    return ( 
-        <nav id='header'>
-            <div className='logo'>
-                <img src={logo}/>
-            </div>
-            <div>
-                <Link to="/">Landing</Link>
-            </div>
-            <div>
-                <Link to="/home">Home</Link>
-            </div>
-            <div>
-                <Link to="/login">Login</Link>
-            </div>
-            <div>
-                <Link to="/register">Register</Link>
-            </div>
+export default function Header() {
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    handleClose();
+    navigate('/')
+  }
+
+  return (
+    <Box sx={{ flexGrow: 1 }} id='header'>
+      
+      <AppBar position="static" sx={{backgroundColor: 'rgba(0, 191, 255, 0.382)', color:'#673F00'}} >
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Stack sx={{flexGrow: 1}} direction="row" alignItems="center" spacing={4}>
+ 
+            <Link to='/'>
+                <Avatar src={logo} sx={{width:60, height:60, margin: 2}}/>
+            </Link>
             
-      </nav>
-     );
+          </Stack>
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <Avatar 
+                    src='https://www.vietnamfineart.com.vn/wp-content/uploads/2023/03/avatar-chill-anime-2.jpg'
+                    sx={{width:40, height:40}}/>
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>
+                    <Link to='/account' style={{textDecoration:'none', color:'black'}}>Profile account</Link>
+                </MenuItem>
+                <MenuItem onClick={handleLogout}>
+                    Log out
+                </MenuItem>
+                
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
-export default Header;
--- frontend/src/pages/landing/Landing.js
+++ frontend/src/pages/landing/Landing.js
@@ -1,9 +1,55 @@
+import { Grid, Stack, ThemeProvider, Typography, createTheme } from '@mui/material';
 import React from 'react';
+import './style.scss'
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+
+const theme = createTheme({
+    palette:{
+        primary:{
+            main: '#439FFF',
+        },
+        secondary:{
+            main: '#00EBBE'
+        },
+        
+    }
+})
+
+const Introduction = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <Stack direction='row' justifyContent='center' mt={2}>
+                <Grid2 container sx={{width:'80%'}} alignItems='center'>
+                    <Grid xs='8'>
+                        <Stack direction='column' alignItems='center' justifyContent='center' className='containerText'>
+                            <Typography variant='h4' mt={5}>
+                                Welcome to Google Classroom!
+                            </Typography>
+                            <Typography variant='h5'>
+                                Come to us, we will bring you the best education experience!
+                            </Typography>
+                        </Stack>
+                    </Grid>
+                        <img
+                            src='https://i.pinimg.com/564x/1c/50/31/1c5031952f93d4bf890253d8f5283549.jpg'/>
+                    <Grid xs='8'>
+
+                    </Grid>
+                </Grid2>
+            </Stack>
+            
+            
+        </ThemeProvider>
+    )
+}
 
 function Landing() {
     return ( 
         <>
-            Landing
+            <div className='introduction'>
+                <Introduction/>
+            </div>
+            
         </>
      );
 }

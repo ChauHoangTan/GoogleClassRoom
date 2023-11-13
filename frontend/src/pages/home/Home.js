2a00474790dc68a351be956fae315ae697c6559a
ChauHoangTan <chauhoangtan6937@gmail.com>
2023-11-13 15:25:16 +0700

    homepage

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,10 +1,80 @@
-import React from 'react';
+import { Grid, Stack, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import SchoolIcon from '@mui/icons-material/School';
+import SystemUpdateAltIcon from '@mui/icons-material/SystemUpdateAlt';
+import SettingsIcon from '@mui/icons-material/Settings';
+import './style.scss'
+import HomePageContent from './homePageContent/HomePageContent';
+const Tabs = ({indexTab, setIndexTab}) => {
+
+    const handleOnclick = (index) => {
+        setIndexTab(index)
+    }
+
+    return ( 
+        <Stack direction='column' sx={{color:'#414756'}}>
+
+            <Stack className='containerPanel'>
+            
+                <Stack direction='row' className={`panel ${indexTab === 0 && 'highlight'}`}
+                     onClick={() => handleOnclick(0)} >
+                    <HomeIcon/> 
+                    <Typography variant='body-1' ml={2}>Home</Typography>
+                </Stack>
+               
+                <Stack direction='row' className={`panel ${indexTab === 1 && 'highlight'}`}
+                    onClick={() => handleOnclick(1)}>
+                    <CalendarTodayIcon/> 
+                    <Typography variant='body-1' ml={2}>Calendar</Typography>
+                </Stack>
+                
+                
+            </Stack>
+         
+            <Stack className='containerPanel '>
+                <Stack direction='row' className={`panel ${indexTab === 2 && 'highlight'}`}
+                    onClick={() => handleOnclick(2)}>
+                    <SchoolIcon/> 
+                    <Typography variant='body-1' ml={2}>Registered</Typography>
+                </Stack>
+            </Stack>
+
+            <Stack className='containerPanel'>
+                
+                <Stack direction='row' className={`panel ${indexTab === 3 && 'highlight'}`}
+                     onClick={() => handleOnclick(3)}>
+                    <SystemUpdateAltIcon/> 
+                    <Typography variant='body-1' ml={2}>Archived class</Typography>
+                </Stack>
+            
+                <Stack direction='row' className={`panel ${indexTab === 4 && 'highlight'}`}
+                     onClick={() => handleOnclick(4)}>
+                    <SettingsIcon/> 
+                    <Typography variant='body-1' ml={2}>Setting</Typography>
+                </Stack>
+                
+            </Stack>
+            
+        </Stack>
+    )
+}
 
 function Home() {
+    const [indexTab, setIndexTab] = useState(0)
+
     return ( 
-        <div>
-            Home
-        </div>
+
+        <Grid container id='home'>
+            <Grid item xs={3} className='grid1'>
+                <Tabs indexTab={indexTab} setIndexTab={setIndexTab}/>
+            </Grid>
+
+            <Grid item xs={9} className='grid2'>
+                <HomePageContent />
+            </Grid>
+        </Grid>
      );
 }
 
---
+++ frontend/src/pages/home/homePageContent/HomePageContent.js
@@ -0,0 +1,50 @@
+import { Grid, Typography, Card, Stack, Avatar } from '@mui/material';
+import React from 'react'
+import './style.scss'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import FolderOpenOutlinedIcon from '@mui/icons-material/FolderOpenOutlined';
+import AssignmentIndOutlinedIcon from '@mui/icons-material/AssignmentIndOutlined';
+
+const CardClass = ({title, tagline, author,}) => {
+    return(
+        <Card className='card'>
+            <Stack direction='column' className='cardHeader'>
+                <Stack direction='row' alignItems='center'><Typography variant='h5' className='text-limit'>{title} </Typography> <MoreVertIcon/> </Stack>
+                <div><Typography variant='body-1' className='text-limit'>{tagline}</Typography></div>
+                <div><Typography variant='body-2' className='text-limit'>{author}</Typography></div>
+
+                <img src='https://i.pinimg.com/564x/a7/3d/b9/a73db9ebfd5e0cb4ec1fc466026b2349.jpg'/>
+                <Avatar className='avatar'/>
+            </Stack>
+
+            <div className='expandBottom'></div>
+            <Stack direction='row' sx={{borderTop:'2px solid rgb(199, 199, 199)', padding:'10px 20px', color:'black'}}
+                justifyContent='end'>
+                <AssignmentIndOutlinedIcon/>
+                <FolderOpenOutlinedIcon sx={{marginLeft:'20px'}}/>
+            </Stack>
+            
+            
+        </Card>
+    )
+}
+
+function HomePageContent() {
+    return ( 
+        <Grid container spacing={2} ml={5}>
+            <Grid item>
+                <CardClass title='2310-CLC-AWP-20KTPM2' 
+                            tagline='Advanced Web Programming'
+                            author='Khánh Nguyễn Huy'/>
+            </Grid>
+
+            <Grid item>
+                <CardClass title='2310-CLC-AWP-20KTPM2' 
+                            tagline='Advanced Web Programming'
+                            author='Khánh Nguyễn Huy'/>
+            </Grid>
+        </Grid>
+     );
+}
+
+export default HomePageContent;

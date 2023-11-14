b80bb919a94db22254bd0196b33156e6358bb059
Vawnndv <dinhvanguyen810@gmail.com>
2023-11-14 15:17:27 +0700

    Create page 404 - Not found

--- frontend/src/pages/noPage/NoPage.js
+++ frontend/src/pages/noPage/NoPage.js
@@ -1,13 +1,22 @@
+// NotFound.js
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Typography, Container, Button, Grid, Paper } from '@mui/material';
 
-function NoPage() {
-    return ( 
-       
-        <div className='d-flex justify-content-center align-items-center'>
-            Error 404
-        </div>
-        
-     );
-}
+const NoPage = () => {
+  return (
+    <Container maxWidth="md" component={Paper} elevation={10} sx={{ my:5, borderRadius: 10 }}>
+      <Typography variant="h1" component="div" gutterBottom sx={{ color: '#465d74' }}>
+        404 - Not Found
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        The page you are looking for might be unavailable or does not exist.
+      </Typography>
+      <Button component={Link} to="/" variant="outlined" sx={{ color: '#465d74', my: 5}}>
+        Go to Home
+      </Button>
+    </Container>
+  );
+};
 
 export default NoPage;

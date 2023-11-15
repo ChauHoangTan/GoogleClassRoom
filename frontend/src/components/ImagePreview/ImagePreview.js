d2e7a172d1d5e4e039ab0ab44afc555f07730696
PhiHung1503 <phihung15032001@gmail.com>
2023-11-15 16:46:08 +0700

    update default user in ImagePreview

--- frontend/src/components/ImagePreview/ImagePreview.js
+++ frontend/src/components/ImagePreview/ImagePreview.js
@@ -5,7 +5,7 @@ const ImagePreview = ({ image, name }) => {
     return (
         <div className="image-preview-container">
             <img
-                src={image ? image : "/images/user.png"}
+                src={image ? image :  "https://www.vietnamfineart.com.vn/wp-content/uploads/2023/03/avatar-chill-anime-2.jpg"}
                 alt={name}
                 className="image-preview"
             />

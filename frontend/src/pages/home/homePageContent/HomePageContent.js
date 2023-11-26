b054eaffa85614ec67bc4ea409183a611eeaa797
ChauHoangTan <chauhoangtan6937@gmail.com>
2023-11-27 00:05:25 +0700

    change home, class details

--- frontend/src/pages/home/homePageContent/HomePageContent.js
+++ frontend/src/pages/home/homePageContent/HomePageContent.js
@@ -1,4 +1,4 @@
-import { Grid, Typography, Card, Stack, Avatar } from '@mui/material'
+import { Grid, Typography, Card, Stack, Avatar, IconButton } from '@mui/material'
 import './style.scss'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 import FolderOpenOutlinedIcon from '@mui/icons-material/FolderOpenOutlined'
@@ -9,22 +9,18 @@ const CardClass = ({ title, tagline, author }) => {
   return (
     <Card className='card'>
       <Stack direction='column' className='cardHeader'>
-        <Stack direction='row' alignItems='center'><Typography variant='h5' className='text-limit'>{title} </Typography> <MoreVertIcon/> </Stack>
-        <div><Typography variant='body-1' className='text-limit'>{tagline}</Typography></div>
-        <div><Typography variant='body-2' className='text-limit'>{author}</Typography></div>
-
         <img src='https://wallpapercave.com/wp/wp6827255.jpg'/>
+        <IconButton><MoreVertIcon/></IconButton>
         <Avatar className='avatar'/>
       </Stack>
 
-      <div className='expandBottom'></div>
-      <Stack direction='row' sx={{ borderTop:'2px solid rgb(199, 199, 199)', padding:'10px 20px', color:'black' }}
-        justifyContent='end'>
-        <AssignmentIndOutlinedIcon sx={{ cursor:'pointer' }}/>
-        <FolderOpenOutlinedIcon sx={{ marginLeft:'20px', cursor:'pointer' }}/>
-      </Stack>
-
-
+      <div className='expandBottom'>
+        <div>
+          <Stack direction='row' alignItems='center'><Typography variant='h6' sx={{ fontWeight:'500' }} className='text-limit'>{title}</Typography></Stack>
+          <div><Typography variant='body-1' className='text-limit'>{tagline}</Typography></div>
+          <div><Typography variant='body-2' className='text-limit'>{author}</Typography></div>
+        </div>
+      </div>
     </Card>
   )
 }

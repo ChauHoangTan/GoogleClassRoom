7bea482945bebe471103d294ee90d8061fa0a099
Nguyen Dinh Van <dinhvanguyen810@gmail.com>
2024-01-04 22:00:14 +0700

    Setup deploy - Update url

--- frontend/src/Context/SocketProvider.js
+++ frontend/src/Context/SocketProvider.js
@@ -21,7 +21,8 @@ function SocketProvider({ children }) {
       withCredentials: true,
       extraHeaders: {
         // 'Access-Control-Allow-Origin': 'http://localhost:3000'
-        'Access-Control-Allow-Origin': 'https://google-class-room-five.vercel.app'
+        // 'Access-Control-Allow-Origin': 'https://google-class-room-five.vercel.app'
+        'Access-Control-Allow-Origin': 'https://nexusedu.vercel.app/'
       }
     }))
   }, [])

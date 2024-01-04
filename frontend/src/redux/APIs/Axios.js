936a833385a1ff23e2ccc75e5e375dd2ccf8df11
Nguyen Dinh Van <dinhvanguyen810@gmail.com>
2024-01-04 21:32:02 +0700

    Setup to deploy

--- frontend/src/redux/APIs/Axios.js
+++ frontend/src/redux/APIs/Axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const Axios = axios.create({
-  baseURL: 'http://localhost:5000/api'
-  // baseURL: 'https://nexusedu.onrender.com/api'
+  // baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://nexusedu.onrender.com/api'
 })
 
 export default Axios

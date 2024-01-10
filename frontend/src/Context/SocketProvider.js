18bb71b07fda421166707398ff1bc54e4e675853
Nguyen Dinh Van <dinhvanguyen810@gmail.com>
2024-01-10 16:35:36 +0700

    Setup Deploy

--- frontend/src/Context/SocketProvider.js
+++ frontend/src/Context/SocketProvider.js
@@ -16,13 +16,13 @@ function SocketProvider({ children }) {
   )
 
   useEffect(() => {
-    // const newSocket = io('https://nexusedu.onrender.com', {
-    const newSocket = io('http://localhost:5000', {
+    const newSocket = io('https://nexusedu.onrender.com', {
+    // const newSocket = io('http://localhost:5000', {
       withCredentials: true,
       extraHeaders: {
-        'Access-Control-Allow-Origin': 'http://localhost:3000'
+        // 'Access-Control-Allow-Origin': 'http://localhost:3000'
         // 'Access-Control-Allow-Origin': 'https://google-class-room-five.vercel.app'
-        // 'Access-Control-Allow-Origin': 'https://nexusedu.vercel.app'
+        'Access-Control-Allow-Origin': 'https://nexusedu.vercel.app'
       }
     })
     setSocket(newSocket)
